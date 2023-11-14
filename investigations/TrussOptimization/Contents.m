a9ba52cf3049840883fa4e056bde1468fece9970
Petr Krysl <pkrysl@ucsd.edu>
2023-11-13 20:18:19 -0800

    add another way of getting to the optimization template for compatibility

---
+++ investigations/TrussOptimization/Contents.m
@@ -0,0 +1,7 @@
+% TRUSSOPTIMIZATION
+%
+% Equivalent to TRUSSOPTIMIZATIONTEMPLATE: It is here for historical
+% reasons.
+%
+% Files
+%   truss_structure_optimization_template - Template for shape and sizing optimization
