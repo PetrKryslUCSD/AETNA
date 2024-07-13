9ce10a8b74c4fa9c8dd56545bc2acae8f2edc861
Petr Krysl <pkrysl@ucsd.edu>
2024-07-12 18:34:51 -0700

    update start

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
-#path="C:\Program Files\MATLAB\R2013b\bin";"C:\Program Files\MATLAB\R2012b\bin";%path%
+#path="C:\Program Files\MATLAB\R2024b\bin";"C:\Program Files\MATLAB\R2012b\bin";%path%
 set MATLAB_PREFDIR=%cd%\profile
 matlab.exe /nosplash /r aetna_init
