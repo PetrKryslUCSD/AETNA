19bca6f5dc27847808322729571fd294be685665
Petr Krysl <pkrysl@ucsd.edu>
2024-07-24 09:26:34 -0700

    add the possibility of controlling the number of iterations

--- src/ODE_suite/integrators/odetrap.m
+++ src/ODE_suite/integrators/odetrap.m
@@ -25,7 +25,8 @@
 %              to the desired step size which kept throughout the
 %              calculation.  The field  RelTol may be set to control the
 %              relative tolerance of the solution components (1e-3 is the
-%              default). 
+%              default). The field MaxIter can be set to control the
+%              maximum iterations taken by the Newton solver.
 %
 % Copyright (C) 2007-2016, Petr Krysl
 % 
@@ -36,6 +37,8 @@ function [ts, ys] = odetrap(rhsfun, tspan, y0, options, varargin)
     options.RelTol =1e-3; 
     try,        options.RelTol=options.(matchfieldname(options, 'RelTol'));
     catch,      options.RelTol =1e-3;    end
+    try,        options.MaxIter=options.(matchfieldname(options, 'MaxIter'));
+    catch,      options.MaxIter =12;    end
     % check whether we are going forward or backward in time
     if (tspan(2)<tspan(1)),h=-abs(h);end
     nsteps = abs(ceil ((tspan(2) - tspan(1)) / h));
@@ -55,7 +58,7 @@ function [ts, ys] = odetrap(rhsfun, tspan, y0, options, varargin)
     for step = 2:nsteps+1
         if (abs(tspan(2)-ts(step-1))<abs(h)),h=tspan(2)-ts(step-1);end
         ts(step) = ts(step-1) + h;
-        ys(step,:) = (Trap_Newton (rhsfun, h, ts(step-1), ys(step-1,:)', options, varargin{:}))';
+        ys(step,:) = (Trap_Newton(rhsfun, h, ts(step-1), ys(step-1,:)', options, varargin{:}))';
     end
     return;
 end
@@ -72,17 +75,17 @@ end
 %   yn     = function value y_n
 %   options= see documentation for odetrap
 %   
-function [yn1] = Trap_Newton (rhsfun, h, tn, yn, options, varargin)
+function [yn1] = Trap_Newton(rhsfun, h, tn, yn, options, varargin)
     tp  = tn + h;
     yp  = yn + h * feval (rhsfun, tp, yn, varargin{:});
     fn=feval(rhsfun, tn, yn, varargin{:});
     yn1 =yp;
-    maximum_iteration = 12;
-    A = -(h/2) *options.drhsfundy +eye (length(yp));
-    for iteration = 1: maximum_iteration
-        nyn1 =yn1 - A\(yn1 - yn - (h/2) * (feval(rhsfun, tp, yn1, varargin{:}) + fn));
+    maximum_iteration = options.MaxIter;
+    A = -(h/2) *options.drhsfundy + eye(length(yp));
+    for iteration = 1:maximum_iteration
+        nyn1 =yn1 - A \ (yn1 - yn - (h/2) * (feval(rhsfun, tp, yn1, varargin{:}) + fn));
         yn1increment=nyn1-yn1;
-        if norm(yn1increment,inf)<options.RelTol*norm(max(max(abs(yn1)), options.threshold),inf)
+        if norm(yn1increment,inf) < options.RelTol*norm(max(max(abs(yn1)), options.threshold),inf)
             yn1 =nyn1; %iteration, 
             return;
         end
